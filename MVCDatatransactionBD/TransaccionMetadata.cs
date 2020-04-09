d0737fdc2196d7ce03e55e5d137f685989a90ea6
malom <malom@LAPTOP-HB5BBVVC>
2020-04-09 16:13:34 -0500

    Actualización de Transaccion Metadata

---
+++ MVCDatatransactionBD/TransaccionMetadata.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCDatatransactionBD
+{
+    [MetadataType(typeof(Transaccion.Metadata))]
+    public partial class Transaccion
+    {
+        sealed class Metadata
+        {
+            [DisplayName("ID")]
+            public int tra_id { get; set; }
+
+            [DisplayName("NumeroCuenta")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(12)]
+            public string tra_accountnumber { get; set; }
+
+            [DisplayName("Beneficiario")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(100)]
+            public string tra_beneficiaryname { get; set; }
+
+            [DisplayName("Banco")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(100)]
+            public string tra_bankname { get; set; }
+
+            [DisplayName("SWIFTCode")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(11)]
+            public string tra_SWIFTCode { get; set; }
+
+            [DisplayName("Cantidad")]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public Nullable<int> tra_amount { get; set; }
+
+            [DisplayName("Fecha")]
+            [DataType(DataType.DateTime)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public Nullable<System.DateTime> tra_datetime { get; set; }
+        }
+    }
+}
