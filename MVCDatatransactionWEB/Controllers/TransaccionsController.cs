c8430052977b6ca183c538561edf57907a56dbfc
malom <malom@LAPTOP-HB5BBVVC>
2020-04-09 15:57:55 -0500

    Creación de Controlador Transacción

---
+++ MVCDatatransactionWEB/Controllers/TransaccionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCDatatransactionBD;
+
+namespace MVCDatatransactionWEB.Controllers
+{
+    public class TransaccionsController : Controller
+    {
+        private MVCDatatransactionEntities db = new MVCDatatransactionEntities();
+
+        // GET: Transaccions
+        public ActionResult Index()
+        {
+            return View(db.Transaccion.ToList());
+        }
+
+        // GET: Transaccions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaccion transaccion = db.Transaccion.Find(id);
+            if (transaccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaccion);
+        }
+
+        // GET: Transaccions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Transaccions/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "tra_id,tra_accountnumber,tra_beneficiaryname,tra_bankname,tra_SWIFTCode,tra_amount,tra_datetime")] Transaccion transaccion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transaccion.Add(transaccion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(transaccion);
+        }
+
+        // GET: Transaccions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaccion transaccion = db.Transaccion.Find(id);
+            if (transaccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaccion);
+        }
+
+        // POST: Transaccions/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "tra_id,tra_accountnumber,tra_beneficiaryname,tra_bankname,tra_SWIFTCode,tra_amount,tra_datetime")] Transaccion transaccion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transaccion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transaccion);
+        }
+
+        // GET: Transaccions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaccion transaccion = db.Transaccion.Find(id);
+            if (transaccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaccion);
+        }
+
+        // POST: Transaccions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Transaccion transaccion = db.Transaccion.Find(id);
+            db.Transaccion.Remove(transaccion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCDatatransactionWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCDatatransactionWEB.Startup))]
+namespace MVCDatatransactionWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
