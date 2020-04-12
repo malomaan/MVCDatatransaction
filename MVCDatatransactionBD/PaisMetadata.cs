f2c6c1cba0250c83daa70c37067cd84a3c20687e
malom <malom@LAPTOP-HB5BBVVC>
2020-04-11 19:42:51 -0500

    Se termino el Objeto Pais

---
+++ MVCDatatransactionBD/PaisMetadata.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVCDatatransactionBD
+{
+    [MetadataType(typeof(Pais.Metadata))]
+    public partial class Pais
+    {
+        sealed class Metadata
+        {
+            [DisplayName("Codigo")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(2)]
+            public string pai_codigo { get; set; }
+
+            [DisplayName("Nombre")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(250)]
+            public string pai_nombre { get; set; }
+
+            [DisplayName("CodigoInternacional")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(20)]
+            public string pai_codigointernacional { get; set; }
+
+            [DisplayName("Predeterminado")]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public Nullable<bool> pai_predeterminado { get; set; }
+        }
+    }
+}
